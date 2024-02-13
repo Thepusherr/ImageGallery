2ce73f8ac4b0a4893d8d205444033bfb46f4f12d
Pusher <spunkspunkik@gmail.com>
2024-02-13 18:58:43 +0200

    adding avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :surname, :email) }
+  #   devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :surname, :email) }
+  # end
 end
