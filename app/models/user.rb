44c10b15ee95578a6ac1274e917ea6482c0ea275
Pusher <spunkspunkik@gmail.com>
2024-02-15 18:23:11 +0200

    adding navbar, adding avatar for registration, edit form

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   validates_presence_of   :avatar
   validates_integrity_of  :avatar
   validates_processing_of :avatar
+  validate :avatar_size_validation
+
   private
   def avatar_size_validation
     errors[:avatar] << "should be less than 500KB" if avatar.size > 0.5.megabytes
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,8 @@ Rails.application.routes.draw do
   devise_for :users
     root to: "home#index"
   end
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
